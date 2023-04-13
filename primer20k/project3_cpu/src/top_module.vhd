4b473575124586f3a72633c1757a4cdd9fd42f2d
Marcin Lapaj <marcin.lapaj@gmail.com>
2023-04-13 12:46:43 +0200

    adding cpu project

---
+++ primer20k/project3_cpu/src/top_module.vhd
@@ -0,0 +1,14 @@
+library IEEE;
+use ieee.std_logic_1164.all;
+
+entity top_module is
+port (
+   CLK: in std_logic
+     );
+end top_module;
+
+architecture basic of top_module is
+
+begin
+
+end basic;
