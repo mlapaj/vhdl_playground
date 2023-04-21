7f609b45dfc52483cb0ad3e24f0a11bef1e644aa
Marcin Lapaj <marcin.lapaj@gmail.com>
2023-04-21 22:16:48 +0200

    new changes

--- ghdl/tutorial03/source/z80system_ram.vhd
+++ ghdl/tutorial03/source/z80system_ram.vhd
@@ -22,36 +22,37 @@ signal data_tmp: std_logic_vector(7 downto 0);
 type std_logic_vector_file is file of mem8_ram_t;
 file file_ram : std_logic_vector_file;
 begin
-    process (clk_i)
-        variable addr: std_logic_vector(15 downto 0);
-    begin
-        if (falling_edge(clk_i)) then
-            if (mreqn_i = '0') then
-                addr := addr_i; 
-                --report "latch addr to tmp " & to_hstring(addr);
-            end if;
-            if (rd_i = '0') then
-                --report "read from addr " & to_hstring(addr);
-                if (unsigned(addr) > 16383) and (unsigned(addr) < 65536) then
-                    report "reading from ram[" & to_hstring(addr) & "]=" & to_hstring(ram_data(to_integer(unsigned(addr))-16384));
-                    data_io <= ram_data(to_integer(unsigned(addr))-16384);
-                else
-                    data_io <= "ZZZZZZZZ";
-                end if;
-            else
-                    data_io <= "ZZZZZZZZ";
-            end if;
-            if (wr_i = '0') then
-                if (unsigned(addr) > 16383) and (unsigned(addr) < 65536) then
-                    ram_data(to_integer(unsigned(addr))-16384) <= data_io;
-                    report "writing to ram[" & to_hstring(addr) & "]=" & to_hstring(data_io) ;
-                end if;
-            end if;
-        end if;
+	process (mreqn_i, rd_i, wr_i)
+		variable addr: std_logic_vector(15 downto 0);
+	begin
+		if (falling_edge(mreqn_i)) then
+			addr := addr_i;
+			if (unsigned(addr) > 16383) and (unsigned(addr) < 65536) then
+				--report "latch addr to tmp " & to_hstring(addr);
+			end if;
+		end if;
+		if (falling_edge(rd_i) and (mreqn_i = '0')) then
+				--report "read from addr " & to_hstring(addr);
+			if (unsigned(addr) > 16383) and (unsigned(addr) < 65536) then
+				report "reading from ram[" & to_hstring(addr) & "]=" &
+				       to_hstring(ram_data(to_integer(unsigned(addr))-16384));
+				data_io <= ram_data(to_integer(unsigned(addr))-16384);
+			else
+				data_io <= "ZZZZZZZZ";
+			end if;
+		elsif (rising_edge(rd_i) or rising_edge(mreqn_i)) then
+			data_io <= "ZZZZZZZZ";
+		end if;
+		if ((falling_edge(wr_i)) and (mreqn_i = '0')) then
+			if (unsigned(addr) > 16383) and (unsigned(addr) < 65536) then
+				ram_data(to_integer(unsigned(addr))-16384) <= data_io;
+				report "writing to ram[" & to_hstring(addr) & "]=" & to_hstring(data_io) ;
+			end if;
+		end if;
     end process;
     process
     begin
-        wait for 20 ms;
+        wait for 800 ms;
         file_open(file_ram, "out_ram.txt",  write_mode);
         report "writing ram to file";
         write(file_ram, ram_data);
--- ghdl/tutorial03/source/z80system_rom.vhd
+++ ghdl/tutorial03/source/z80system_rom.vhd
@@ -18,22 +18,23 @@ end entity;
 architecture basic of z80system_rom is
 signal data_tmp: std_logic_vector(7 downto 0);
 begin
-    process (clk_i)
-    variable addr : std_logic_vector(15 downto 0);
-    begin
-        if (falling_edge(clk_i)) then
-            if (mreqn_i = '0') then
-                addr := addr_i; 
-                --report "latch addr to tmp " & to_hstring(addr);
-            end if;
-            if (rd_i = '0') then
-                if (unsigned(addr) < 16384) then
-                    data_io <= rom_image(to_integer(unsigned(addr)));
-                    --report "read rom[ " & to_hstring(addr) & "] =" & to_hstring(data_io);
-                else
-                    data_io <= "ZZZZZZZZ";
-                end if;
-            end if;
-        end if;
-    end process;
+	process (mreqn_i, rd_i)
+		variable addr : std_logic_vector(15 downto 0);
+	begin
+		if (falling_edge(mreqn_i)) then
+			addr := addr_i; 
+			--report "latch addr to tmp " & to_hstring(addr);
+		end if;
+		if (falling_edge(rd_i)) and ((mreqn_i = '0') or (falling_edge(mreqn_i))) then
+			if (unsigned(addr) < 16384) then
+				data_io <= rom_image(to_integer(unsigned(addr)));
+				-- report "read rom[ " & to_hstring(addr) & "] =" & 
+				--        to_hstring(rom_image(to_integer(unsigned(addr))));
+			else
+				data_io <= "ZZZZZZZZ";
+			end if;
+		elsif (rising_edge(rd_i) or rising_edge(mreqn_i)) then
+			data_io <= "ZZZZZZZZ";
+		end if;
+	end process;
 end;
